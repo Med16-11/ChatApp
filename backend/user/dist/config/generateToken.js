687369ee8dda4ad3e966e027680cf2d98d9afe3c
Medhavi Singh <medhavisingh@Mac.dlink>
2026-03-24 21:34:48 +0530

    Remove dist from tracking

--- backend/user/dist/config/generateToken.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-dotenv.config();
-const JWT_SCRET = process.env.JWT_SECRET;
-export const generateToken = (user) => {
-    return jwt.sign({ user }, JWT_SCRET, { expiresIn: "15d" });
-};
-//# sourceMappingURL=generateToken.js.map
